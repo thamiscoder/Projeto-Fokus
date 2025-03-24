096f13337c16c4781165c8814357a2159072989e
thamiscoder <thamirisoli34@gmail.com>
2025-03-24 13:21:33 -0300

    Diminuindo o codigo

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
 const image = document.querySelector('.app__image');
 const text = document.querySelector('.app__title');
-const timer = document.getElementById('#timer');
+const timer = document.querySelector('#timer');
 const startPause = document.querySelector('.app__card-primary-button');
 const tempoDeFoco = 1500;
 const tempoCurto = 300;
@@ -12,16 +12,18 @@ const tempoLongo = 900;
 
 
 focoBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
-    image.setAttribute('src', '/imagens/foco.png');
+    alterarContexto('foco');
 })
 
 curtoBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto');
-    image.setAttribute('src', '/imagens/descanso-curto.png');
+    alterarContexto('descanso-curto');
 })
 
 longoBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo');
-    image.setAttribute('src', '/imagens/descanso-longo.png');
+    alterarContexto('descanso-longo');
 })
+
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    image.setAttribute('src', `/imagens/${contexto}.png`);
+}
