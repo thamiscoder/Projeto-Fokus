0abe539cb9b498b2fc4efb4020a24691ec5a2871
thamiscoder <thamirisoli34@gmail.com>
2025-03-24 12:34:45 -0300

    Outras variaveis

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const html = document.querySelector('html');
 const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
-
-let image = document.querySelector('.app__image');
-let text = document.querySelector('.app__title');
-let timer = document.querySelector('#timer');
-
+const image = document.querySelector('.app__image');
+const text = document.querySelector('.app__title');
+const timer = document.getElementById('#timer');
+const startPause = document.querySelector('.app__card-primary-button');
+const tempoDeFoco = 1500;
+const tempoCurto = 300;
+const tempoLongo = 900;
 
 
 focoBtn.addEventListener('click', () => {
